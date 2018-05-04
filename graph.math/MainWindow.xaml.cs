24d090830b79ebda5a15dcfd28e4ec72d5a3716b
mig1024 <mig1024@yandex.ru>
2018-05-04 21:35:13 +0300

    drawLine / drawLineAbsolute

--- graph.math/MainWindow.xaml.cs
+++ graph.math/MainWindow.xaml.cs
@@ -23,30 +23,55 @@ namespace graph.math
             InitializeComponent();
         }
 
-        void graphPlaceBackground(int scale)
+        private double getXPoint(double xPoint)
         {
-            var width = graphPlace.ActualWidth;
-            var height = graphPlace.ActualHeight;
-
-            double wcenter = width / 2; 
-            double hcenter = height / 2;
-
-            graphPlace.Children.Clear();
+            double width = graphPlace.ActualWidth;
+            double wcenter = width / 2;
+            int scale = (int)graphScale.Value;
+            int centerPoint = 0;
 
             for (int x = scale; x < width; x += scale)
                 if ((x >= wcenter) && (x < wcenter + scale))
-                    AddLineToBackground(x, 0, x, height, true);
-                else
-                    AddLineToBackground(x, 0, x, height);
+                {
+                    centerPoint = x;
+                }
 
-            for (int y = scale; y < height; y += scale)
-                if ((y > hcenter) && (y <hcenter + scale) )
-                    AddLineToBackground(0, y, width, y, true);
-                else
-                    AddLineToBackground(0, y, width, y);
+            double newPoint = centerPoint + (xPoint * scale);
+
+            return newPoint;
         }
 
-        void AddLineToBackground(double x1, double y1, double x2, double y2, bool coordinate = false)
+        private double getYPoint(double yPoint)
+        {
+            double height = graphPlace.ActualHeight;
+            double hcenter = height / 2;
+            int scale = (int)graphScale.Value;
+            int centerPoint = 0;
+
+            for (int x = scale; x < height; x += scale)
+                if ((x >= hcenter) && (x < hcenter + scale))
+                {
+                    centerPoint = x;
+                }
+
+            double newPoint = centerPoint - (yPoint * scale);
+
+            return newPoint;
+        }
+
+        private void drawLine(double x1, double y1, double x2, double y2, Brush color, int width = 1)
+        {
+            drawLineAbsolute(
+                x1: getXPoint(x1),
+                y1: getYPoint(y1),
+                x2: getXPoint(x2),
+                y2: getYPoint(y2),
+                color: color,
+                width: width
+           );
+        }
+
+        private void drawLineAbsolute(double x1, double y1, double x2, double y2, Brush color, int width = 1)
         {
             var line = new Line()
             {
@@ -54,12 +79,35 @@ namespace graph.math
                 Y1 = y1,
                 X2 = x2,
                 Y2 = y2,
-                Stroke = (coordinate ? Brushes.SkyBlue : Brushes.DarkGray ),
-                StrokeThickness = (coordinate ? 2 : 1),
+                Stroke = color,
+                StrokeThickness = width,
                 SnapsToDevicePixels = true
             };
             line.SetValue(RenderOptions.EdgeModeProperty, EdgeMode.Aliased);
             graphPlace.Children.Add(line);
+        }
+
+        void graphPlaceBackground(int scale)
+        {
+            var width = graphPlace.ActualWidth;
+            var height = graphPlace.ActualHeight;
+            
+            double wcenter = width / 2; 
+            double hcenter = height / 2;
+
+            graphPlace.Children.Clear();
+
+            for (int x = scale; x < width; x += scale)
+                if ((x >= wcenter) && (x < wcenter + scale))
+                    drawLineAbsolute(x, 0, x, height, Brushes.SkyBlue);
+                else
+                    drawLineAbsolute(x, 0, x, height, Brushes.DarkGray);
+
+            for (int y = scale; y < height; y += scale)
+                if ((y > hcenter) && (y <hcenter + scale))
+                    drawLineAbsolute(0, y, width, y, Brushes.SkyBlue);
+                else
+                    drawLineAbsolute(0, y, width, y, Brushes.DarkGray);
         }
 
         private void graphPlace_Loaded(object sender, RoutedEventArgs e)
@@ -75,6 +123,14 @@ namespace graph.math
         private void graphScale_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             graphPlaceBackground((int)graphScale.Value);
+        }
+
+        private void buttonPlay_Click(object sender, RoutedEventArgs e)
+        {
+            drawLine(0, 0, 5, 5, Brushes.Yellow);
+            drawLine(0, 0, -5, 5, Brushes.Red, 2);
+            drawLine(0, 0, -5, -5, Brushes.Green, 3);
+            drawLine(0, 0, 5, -5, Brushes.Blue, 4);
         }
     }
 }
