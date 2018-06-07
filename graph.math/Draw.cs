7923d7213294ecf92c68a07e3ca2babf609af134
mig1024 <mig1024@yandex.ru>
2018-06-07 20:26:58 +0300

    MainWindow <-> Draw

---
+++ graph.math/Draw.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace graph.math
+{
+    class Draw
+    {
+        public delegate double simpleFunction(double x);
+
+        public static Canvas graphPlace;
+        public static Slider graphScale;
+        public static double xMove = 0;
+        public static double yMove = 0;
+
+        public static bool graphPlaceIsReady = false;
+
+        private static int getValueMax(bool negativeNumber = false, bool isY = false)
+        {
+            int scale = (int)graphScale.Value;
+            double size = (isY ? graphPlace.ActualHeight : graphPlace.ActualWidth);
+
+            int centerPoint = (int)(Draw.getCenter(size, xMove) / scale);
+
+            if (isY) negativeNumber = !negativeNumber;
+
+            if (!negativeNumber)
+                return (int)(size / scale) - centerPoint;
+            else
+                return -1 * (centerPoint + 1);
+        }
+
+        public static double getCenter(double actualSize, double xyMove)
+        {
+            double center = xyMove + (actualSize / 2);
+            int scale = (int)graphScale.Value;
+
+            double centerPoint = 0;
+
+            for (double x = center % scale; x < actualSize; x += scale)
+                if ((x >= center) && (x < center + scale))
+                    centerPoint = x;
+
+            return centerPoint;
+        }
+
+        private static double getPoint(double xyPoint, double actualSize, double xyMove, bool isY)
+        {
+            double centerPoint = getCenter(actualSize, xyMove);
+            int scale = (int)graphScale.Value;
+            double newPoint = centerPoint + ((xyPoint * scale) * (isY ? -1 : 1));
+
+            return newPoint;
+        }
+
+        public static double getXPoint(double xPoint)
+        {
+            return getPoint(
+                xyPoint: xPoint,
+                actualSize: graphPlace.ActualWidth,
+                xyMove: xMove,
+                isY: false
+            );
+        }
+
+        public static double getYPoint(double yPoint)
+        {
+            return getPoint(
+                xyPoint: yPoint,
+                actualSize: graphPlace.ActualHeight,
+                xyMove: yMove,
+                isY: true
+            );
+        }
+
+        public static bool drawPointAbsolute(double x, double y, Brush color, int width = 2)
+        {
+            Point point = new Point(x, y);
+            Ellipse elipse = new Ellipse();
+
+            elipse.Width = width;
+            elipse.Height = width;
+
+            elipse.StrokeThickness = 2;
+            elipse.Stroke = color;
+            elipse.Margin = new Thickness(point.X - (width / 2), point.Y - (width / 2), 0, 0);
+
+            graphPlace.Children.Add(elipse);
+
+            return true;
+        }
+
+        public static bool drawPoint(double x, double y, Brush color, int width = 2)
+        {
+            drawPointAbsolute(
+                x: getXPoint(x),
+                y: getYPoint(y),
+                color: color,
+                width: width
+            );
+
+            return true;
+        }
+
+        public static bool drawPoint(int[] coordinate, Brush color, int width = 2)
+        {
+            drawPoint(
+                x: coordinate[0],
+                y: coordinate[1],
+                color: color,
+                width: width
+            );
+
+            return true;
+        }
+
+        public static bool drawLine(int[] coordinate, Brush color, int width = 1, bool dash = false)
+        {
+            drawLine(
+                x1: coordinate[0],
+                y1: coordinate[1],
+                x2: coordinate[2],
+                y2: coordinate[3],
+                color: color,
+                width: width,
+                dash: dash
+            );
+
+            return true;
+        }
+
+        public static bool drawLine(double x1, double y1, double x2, double y2,
+            Brush color, int width = 1, bool dash = false)
+        {
+            drawLineAbsolute(
+                x1: getXPoint(x1),
+                y1: getYPoint(y1),
+                x2: getXPoint(x2),
+                y2: getYPoint(y2),
+                color: color,
+                width: width,
+                dash: dash
+            );
+
+            return true;
+        }
+
+        public static bool drawLineAbsolute(double x1, double y1, double x2, double y2,
+            Brush color, int width = 1, bool dash = false)
+        {
+            var line = new Line()
+            {
+                X1 = x1,
+                Y1 = y1,
+                X2 = x2,
+                Y2 = y2,
+                Stroke = color,
+                StrokeThickness = width,
+                SnapsToDevicePixels = true
+            };
+
+            if (dash)
+            {
+                line.StrokeDashArray = new DoubleCollection() { 4 };
+            }
+
+            line.SetValue(RenderOptions.EdgeModeProperty, EdgeMode.Aliased);
+            graphPlace.Children.Add(line);
+
+            return true;
+        }
+
+        public static bool drawSimpleFuntion(simpleFunction function, int[] param)
+        {
+            if (param.Length == 0)
+                for (double x = getValueMax(negativeNumber: true); x < getValueMax(); x += 0.05)
+                    drawPoint(x, function.Invoke(x), Brushes.White, 1);
+
+            else if (param.Length == 1)
+                drawPoint(param[0], function.Invoke(param[0]), Brushes.Red);
+
+            else return false;
+
+            return true;
+        }
+
+        public static bool drawSumVector(string[] param)
+        {
+            if (param.Length != 2) return false;
+
+            Vector v1 = new Vector();
+            v1 = Vector.allVectors[param[0].ToString()];
+
+            Vector v2 = new Vector();
+            v2 = Vector.allVectors[param[1].ToString()];
+
+            double x1 = v1.x1 + v2.x1;
+            double y1 = v1.y1 + v2.y1;
+            double x2 = v1.x2 + v2.x2;
+            double y2 = v1.y2 + v2.y2;
+
+            drawLine(v1.x2, v1.y2, x2, y2, Brushes.Red, dash: true);
+            drawLine(v2.x2, v2.y2, x2, y2, Brushes.Red, dash: true);
+
+            drawLine(v1.x1, v1.y1, x1, y1, Brushes.Red, dash: true);
+            drawLine(v2.x1, v2.y1, x1, y1, Brushes.Red, dash: true);
+
+            drawLine(x1, y1, x2, y2, Brushes.Red);
+
+            return true;
+        }
+
+        public static void graphPlaceBackground(int scale)
+        {
+            if (!graphPlaceIsReady) return;
+
+            var width = graphPlace.ActualWidth;
+            var height = graphPlace.ActualHeight;
+
+            double wCenter = xMove + (width / 2);
+            double hCenter = yMove + (height / 2);
+
+            double xMargin = wCenter % scale;
+            double yMargin = hCenter % scale;
+
+            graphPlace.Children.Clear();
+
+            for (double x = xMargin; x < width; x += scale)
+            {
+                Brush color = ((x >= wCenter) && (x < wCenter + scale) ? Brushes.SkyBlue : Brushes.DimGray);
+                drawLineAbsolute(x, 0, x, height, color);
+            }
+
+            for (double y = yMargin; y < height; y += scale)
+            {
+                Brush color = ((y >= hCenter) && (y < hCenter + scale) ? Brushes.SkyBlue : Brushes.DimGray);
+                drawLineAbsolute(0, y, width, y, color);
+            }
+        }
+
+        public static int getValueMax(Canvas graphPlace, Slider graphScale, double xMove, double yMove,
+            bool negativeNumber = false, bool isY = false)
+        {
+            int scale = (int)graphScale.Value;
+            double size = (isY ? graphPlace.ActualHeight : graphPlace.ActualWidth);
+
+            int centerPoint = (int)(getCenter(size, xMove) / scale);
+
+            if (isY) negativeNumber = !negativeNumber;
+
+            if (!negativeNumber)
+                return (int)(size / scale) - centerPoint;
+            else
+                return -1 * (centerPoint + 1);
+        }
+    }
+}
--- graph.math/MainWindow.xaml.cs
+++ graph.math/MainWindow.xaml.cs
@@ -27,168 +27,15 @@ namespace graph.math
 
         bool moveGraphPlace = false;
         bool graphDrawAlready = false;
-        double xMove = 0;
-        double yMove = 0;
 
         public MainWindow()
         {
             InitializeComponent();
             hightlightText = graphText.SelectionBrush;
-        }
+            Draw.graphPlace = graphPlace;
+            Draw.graphScale = graphScale;
 
-        private int getValueMax(bool negativeNumber = false, bool isY = false)
-        {
-            int scale = (int)graphScale.Value;
-            double size = ( isY ? graphPlace.ActualHeight : graphPlace.ActualWidth );
-
-            int centerPoint = (int)(getCenter(size, xMove) / scale);
-
-            if (isY) negativeNumber = !negativeNumber;
-
-            if (!negativeNumber)
-                return (int)(size / scale) - centerPoint;
-            else
-                return -1 * (centerPoint + 1);
-        }
-
-        private double getCenter(double actualSize, double xyMove)
-        {
-            double center = xyMove + (actualSize / 2);
-            int scale = (int)graphScale.Value;
-
-            double centerPoint = 0;
-
-            for (double x = center % scale; x < actualSize; x += scale)
-                if ((x >= center) && (x < center + scale))
-                    centerPoint = x;
-
-            return centerPoint;
-        }
-
-        private double getPoint(double xyPoint, double actualSize, double xyMove, bool isY)
-        {
-            double centerPoint = getCenter(actualSize, xyMove);
-            int scale = (int)graphScale.Value;
-            double newPoint = centerPoint + ((xyPoint * scale) * (isY ? 1 : -1));
-
-            return newPoint;
-        }
-
-        private double getXPoint(double xPoint)
-        {
-            return getPoint(
-                xyPoint: xPoint,
-                actualSize: graphPlace.ActualWidth,
-                xyMove: xMove,
-                isY: true
-            );
-        }
-
-        private double getYPoint(double yPoint)
-        {
-            return getPoint(
-                xyPoint: yPoint,
-                actualSize: graphPlace.ActualHeight,
-                xyMove: yMove,
-                isY: false
-            );
-        }
-
-        private bool drawPoint(double x, double y, Brush color, int width = 2)
-        {
-            drawPointAbsolute(
-                x: getXPoint(x),
-                y: getYPoint(y),
-                color: color,
-                width: width
-            );
-
-            return true;
-        }
-
-        private bool drawPoint(int[] coordinate, Brush color, int width = 2)
-        {
-            drawPoint(
-                x: coordinate[0],
-                y: coordinate[1],
-                color: color,
-                width: width
-            );
-
-            return true;
-        }
-
-        private bool drawPointAbsolute(double x, double y, Brush color, int width = 2)
-        {
-            Point point = new Point(x, y);
-            Ellipse elipse = new Ellipse();
-
-            elipse.Width = width;
-            elipse.Height = width;
-
-            elipse.StrokeThickness = 2;
-            elipse.Stroke = color;
-            elipse.Margin = new Thickness(point.X - (width / 2), point.Y - (width / 2), 0, 0);
-
-            graphPlace.Children.Add(elipse);
-
-            return true;
-        }
-
-        private bool drawLine(int[] coordinate, Brush color, int width = 1, bool dash = false)
-        {
-            drawLine(
-                x1: coordinate[0],
-                y1: coordinate[1],
-                x2: coordinate[2],
-                y2: coordinate[3],
-                color: color,
-                width: width,
-                dash: dash
-            );
-
-            return true;
-        }
-
-        private bool drawLine(double x1, double y1, double x2, double y2,
-            Brush color, int width = 1, bool dash = false)
-        {
-            drawLineAbsolute(
-                x1: getXPoint(x1),
-                y1: getYPoint(y1),
-                x2: getXPoint(x2),
-                y2: getYPoint(y2),
-                color: color,
-                width: width,
-                dash: dash
-            );
-
-            return true;
-        }
-
-        private bool drawLineAbsolute(double x1, double y1, double x2, double y2,
-            Brush color, int width = 1, bool dash = false)
-        {
-            var line = new Line()
-            {
-                X1 = x1,
-                Y1 = y1,
-                X2 = x2,
-                Y2 = y2,
-                Stroke = color,
-                StrokeThickness = width,
-                SnapsToDevicePixels = true
-            };
-
-            if (dash)
-            {
-                line.StrokeDashArray = new DoubleCollection() { 4 };
-            }
-
-            line.SetValue(RenderOptions.EdgeModeProperty, EdgeMode.Aliased);
-            graphPlace.Children.Add(line);
-
-            return true;
+            Draw.graphPlaceIsReady = true;
         }
 
         string[] parseGraphText()
@@ -235,44 +82,10 @@ namespace graph.math
             return true;
         }
 
-        bool drawSimpleFuntion(simpleFunction function, int[] param)
+        private void graphPlaceReDraw()
         {
-            if (param.Length == 0)
-                for (double x = getValueMax(negativeNumber: true); x < getValueMax(); x += 0.05)
-                    drawPoint(x, function.Invoke(x), Brushes.White, 1);
-
-            else if (param.Length == 1)
-                drawPoint(param[0], function.Invoke(param[0]), Brushes.Red);
-
-            else return false;
-
-            return true;
-        }
-
-        bool drawSumVector(string[] param)
-        {
-            if (param.Length != 2) return false;
-
-            Vector v1 = new Vector();
-            v1 = Vector.allVectors[param[0].ToString()];
-
-            Vector v2 = new Vector();
-            v2 = Vector.allVectors[param[1].ToString()];
-
-            double x1 = v1.x1 + v2.x1;
-            double y1 = v1.y1 + v2.y1;
-            double x2 = v1.x2 + v2.x2;
-            double y2 = v1.y2 + v2.y2;
-
-            drawLine(v1.x2, v1.y2, x2, y2, Brushes.Red, dash: true);
-            drawLine(v2.x2, v2.y2, x2, y2, Brushes.Red, dash: true);
-
-            drawLine(v1.x1, v1.y1, x1, y1, Brushes.Red, dash: true);
-            drawLine(v2.x1, v2.y1, x1, y1, Brushes.Red, dash: true);
-
-            drawLine(x1, y1, x2, y2, Brushes.Red);
-
-            return true;
+            Draw.graphPlaceBackground((int)graphScale.Value);
+            if (graphDrawAlready) drawAlgorithm();
         }
 
         bool drawAlgorithmLine(string algorithmLine, int line)
@@ -310,12 +123,12 @@ namespace graph.math
                 if (varName != "")
                     Vector.createNewVector(varName, p);
 
-                return drawLine(p, Brushes.White);
+                return Draw.drawLine(p, Brushes.White);
             }
 
             else if (algorithmLine.IndexOf("sum") > -1)
             {
-                return drawSumVector(parseStrParam(algorithmLine));
+                return Draw.drawSumVector(parseStrParam(algorithmLine));
             }
 
             else if (algorithmLine.IndexOf("point") > -1)
@@ -324,22 +137,22 @@ namespace graph.math
 
                 if (p.Length != 2) return false;
 
-                return drawPoint(p[0], p[1], Brushes.Red);
+                return Draw.drawPoint(p, Brushes.Red);
             }
 
             else if (algorithmLine.IndexOf("sin") > -1)
             {
-                return drawSimpleFuntion(Math.Sin, parseParam(algorithmLine));
+                return Draw.drawSimpleFuntion(Math.Sin, parseParam(algorithmLine));
             }
 
             else if (algorithmLine.IndexOf("cos") > -1)
             {
-                return drawSimpleFuntion(Math.Cos, parseParam(algorithmLine));
+                return Draw.drawSimpleFuntion(Math.Cos, parseParam(algorithmLine));
             }
 
             else if (algorithmLine.IndexOf("tg") > -1)
             {
-                return drawSimpleFuntion(Math.Tan, parseParam(algorithmLine));
+                return Draw.drawSimpleFuntion(Math.Tan, parseParam(algorithmLine));
             }
 
             else
@@ -370,38 +183,7 @@ namespace graph.math
             return true;
         }
 
-        void graphPlaceBackground(int scale)
-        {
-            var width = graphPlace.ActualWidth;
-            var height = graphPlace.ActualHeight;
-            
-            double wCenter = xMove + ( width / 2 ); 
-            double hCenter = yMove + ( height / 2 );
-
-            double xMargin = wCenter % scale;
-            double yMargin = hCenter % scale;
-
-            graphPlace.Children.Clear();
-
-            for (double x = xMargin; x < width; x += scale)
-            {
-                Brush color = ((x >= wCenter) && (x < wCenter + scale) ? Brushes.SkyBlue : Brushes.DimGray);
-                drawLineAbsolute(x, 0, x, height, color);
-            }
-
-            for (double y = yMargin; y < height; y += scale)
-            {
-                Brush color = ((y >= hCenter) && (y < hCenter + scale) ? Brushes.SkyBlue : Brushes.DimGray);
-                drawLineAbsolute(0, y, width, y, color);
-            }
-        }
-
-        private void graphPlaceReDraw()
-        {
-            graphPlaceBackground((int)graphScale.Value);
-            if (graphDrawAlready) drawAlgorithm();
-        }
-
+        
         private void graphPlace_Loaded(object sender, RoutedEventArgs e)
         {
             graphPlaceReDraw();
@@ -427,7 +209,7 @@ namespace graph.math
         private void buttonClear_Click(object sender, RoutedEventArgs e)
         {
             graphPlace.Children.Clear();
-            graphPlaceBackground((int)graphScale.Value);
+            Draw.graphPlaceBackground((int)graphScale.Value);
             graphDrawAlready = false;
         }
 
@@ -445,8 +227,8 @@ namespace graph.math
             {
                 Control c = sender as Control;
 
-                xMove = e.GetPosition(null).X - p.X;
-                yMove = e.GetPosition(null).Y - p.Y;
+                Draw.xMove = e.GetPosition(null).X - p.X;
+                Draw.yMove = e.GetPosition(null).Y - p.Y;
 
                 graphPlaceReDraw();
             }
